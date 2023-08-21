2835c055b36a46c1709e345a7bcb636a66fd2c92
B_n <78348022+boonblablabla@users.noreply.github.com>
2023-08-21 11:29:22 +0700

    Initial Commit

---
+++ src/main/java/ku/cs/cafeapp/CafeAppApplication.java
@@ -0,0 +1,13 @@
+package ku.cs.cafeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ku/cs/cafeapp/CafeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ku.cs.cafeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
