5d87135c65d51dc7911f3063c2720b72b151f84c
B_n <78348022+boonblablabla@users.noreply.github.com>
2023-08-21 12:07:36 +0700

    Add MemberRepository

---
+++ src/main/java/ku/cs/cafeapp/repository/MemberRepository.java
@@ -0,0 +1,17 @@
+package ku.cs.cafeapp.repository;
+
+import ku.cs.cafeapp.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, UUID> {
+
+    /*
+     * This is an equivalent of
+     * SELECT * FROM Member WHERE username = 'username'
+     */
+    Member findByUsername(String username);
+}
