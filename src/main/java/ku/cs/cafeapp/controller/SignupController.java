96d5e4e7b9b93a955bd86191878adbd351dc69b2
B_n <78348022+boonblablabla@users.noreply.github.com>
2023-08-21 12:34:48 +0700

    Add SignupController

---
+++ src/main/java/ku/cs/cafeapp/controller/SignupController.java
@@ -0,0 +1,33 @@
+package ku.cs.cafeapp.controller;
+
+import ku.cs.cafeapp.entity.Member;
+import ku.cs.cafeapp.service.SignupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class SignupController {
+
+    @Autowired private SignupService signupService;
+
+    @GetMapping("/signup")
+    public String getSignupPage() {
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String signupUser(@ModelAttribute Member user, Model model) {
+        if (signupService.isUsernameAvailable(user.getUsername())) {
+            signupService.createUser(user);
+            model.addAttribute("signupSuccess", this);
+        } else {
+            model.addAttribute("signupError", "Username not available");
+        }
+
+        return "signup";
+    }
+}
