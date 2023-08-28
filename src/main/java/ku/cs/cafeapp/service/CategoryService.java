f6d4449ef5c1fd8b527219cf020f3cacdb416736
B_n <78348022+boonblablabla@users.noreply.github.com>
2023-08-28 12:53:08 +0700

    Add CategoryService

---
+++ src/main/java/ku/cs/cafeapp/service/CategoryService.java
@@ -0,0 +1,26 @@
+package ku.cs.cafeapp.service;
+
+import ku.cs.cafeapp.entity.Category;
+import ku.cs.cafeapp.model.CategoryRequest;
+import ku.cs.cafeapp.repository.CategoryRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    @Autowired private CategoryRepository categoryRepository;
+    @Autowired private ModelMapper modelMapper;
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public void createCategory(CategoryRequest category) {
+        Category record = modelMapper.map(category, Category.class);
+        categoryRepository.save(record);
+    }
+
+}
