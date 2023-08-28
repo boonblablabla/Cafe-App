cfbe1fbb1a9e0003c39a72fbdc2d3212879550b6
B_n <78348022+boonblablabla@users.noreply.github.com>
2023-08-28 12:53:16 +0700

    Add CategoryController

---
+++ src/main/java/ku/cs/cafeapp/controller/CategoryController.java
@@ -0,0 +1,30 @@
+package ku.cs.cafeapp.controller;
+
+import ku.cs.cafeapp.model.CategoryRequest;
+import ku.cs.cafeapp.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/categories")
+public class CategoryController {
+    @Autowired private CategoryService categoryService;
+
+    @GetMapping("/add")
+    public String getCategoryForm(Model model) {
+        return "category-add";
+    }
+
+    @PostMapping("/add")
+    public String createCategory(@ModelAttribute CategoryRequest category,
+                                 Model model) {
+        categoryService.createCategory(category);
+        model.addAttribute("categories", categoryService.getAllCategories());
+        return "redirect:/menus";
+    }
+}
